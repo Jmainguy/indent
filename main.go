eb2835e1b7ff71c5cb2699e60cc4344f780f202d
Jonathan Mainguy <jon@soh.re>
2021-04-15 15:53:51 -0400

    remove comments and log

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 )
@@ -16,27 +15,16 @@ func main() {
 
 	flag.Parse()
 
-	// os.Open() opens specific file in
-	// read-only mode and this return
-	// a pointer of type os.
 	file, err := os.Open(*filenamePtr)
 
 	if err != nil {
-		log.Fatalf("failed to open")
-
+		fmt.Println("failed to open")
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	// The bufio.NewScanner() function is called in which the
-	// object os.File passed as its parameter and this returns a
-	// object bufio.Scanner which is further used on the
-	// bufio.Scanner.Split() method.
 	scanner := bufio.NewScanner(file)
 
-	// The bufio.ScanLines is used as an
-	// input to the method bufio.Scanner.Split()
-	// and then the scanning forwards to each
-	// new line using the bufio.Scanner.Scan()
-	// method.
 	scanner.Split(bufio.ScanLines)
 	var text []string
 
@@ -44,12 +32,8 @@ func main() {
 		text = append(text, scanner.Text())
 	}
 
-	// The method os.File.Close() is called
-	// on the os.File object to close the file
 	file.Close()
 
-	// and then a loop iterates through
-	// and prints each of the slice values.
 	indention := strings.Repeat(" ", *indentPtr)
 	for _, line := range text {
 		fmt.Printf("%s%s\n", indention, line)
